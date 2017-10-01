f374329f7548c73289775e8c107addf4e4a37693
haoxuancui <haoxuancui@10.0.0.174>
2017-10-01 17:49:01 -0600

    second

--- fighting games/src/fightinggameprocess.java
+++ fighting games/src/fightinggameprocess.java
@@ -9,9 +9,13 @@ public class fightinggameprocess
 			{
 				fighterinformation.fillFighterInformation();
 				fighterintroduce();
+				playerchoosing();
+				computerchooseactions();
 				
 
 			}
+		
+
 		private static void fighterintroduce()
 			{
 				System.out.println("Welcome to the fighting game, choose your fighter");
@@ -19,11 +23,30 @@ public class fightinggameprocess
 				Scanner userinput = new Scanner(System.in);
 				System.out.println("Which fighter do you want to choose?");
 				int userChooseFighter = userinput.nextInt();
+				int computerChoice= (int)(Math.random()*3)+0;
 				
 				System.out.println("Your fighter is "+fighterinformation.fightInformation.get(userChooseFighter).getFighterName());
+				System.out.println("Your opponent is "+fighterinformation.fightInformation.get(computerChoice).getFighterName());
 				System.out.println("Let's Fight!!!");
+				
+			}
+		private static void computerchooseactions()
+			{
+				int computerActions= (int)(Math.random()*2)+1;
+				if (computerActions==1)
+					{
+						int fighting=fighterinformation.fightInformation.get(0).getAttack();
+					}
+			}
+		private static void playerchoosing()
+			{
+				Scanner userinput1 = new Scanner(System.in);
+				System.out.println("Do you want to fight or defnece?");
+				System.out.println("(1) fight  (2) defence");
+				int userChooseAction = userinput1.nextInt();
 				
 				
 			}
+		
 
 	}
