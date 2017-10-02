7c812d034b1548010a61a19a463d938e65d7a3f9
haoxuan.cui <haoxuan.cui@mu-c1-16.mullen.pvt.k12.co.us>
2017-10-02 09:04:12 -0600

    finish it tonight

--- fighting games/src/fightinggameprocess.java
+++ fighting games/src/fightinggameprocess.java
@@ -5,6 +5,10 @@ public class fightinggameprocess
 	{
 
 		static ArrayList<fighterdatabase> printFighter= new ArrayList<fighterdatabase>();
+		static int computerFighterChoice= (int)(Math.random()*3)+0;
+		static int computerDamage=0;
+		static int userChooseFighter=0;
+		static String computerActionChoice;
 		public static void main(String[] args)
 			{
 				fighterinformation.fillFighterInformation();
@@ -18,33 +22,60 @@ public class fightinggameprocess
 
 		private static void fighterintroduce()
 			{
-				System.out.println("Welcome to the fighting game, choose your fighter");
+				System.out.println("Welcome to the fighting game, choose your fighter!!!");
 				System.out.println("0 "+fighterinformation.fightInformation.get(0).getFighterName()+"  1 "+fighterinformation.fightInformation.get(1).getFighterName()+"  2 "+fighterinformation.fightInformation.get(2).getFighterName());
 				Scanner userinput = new Scanner(System.in);
+				int userchoosefighter = userinput.nextInt();
+				userChooseFighter=userchoosefighter;
 				System.out.println("Which fighter do you want to choose?");
-				int userChooseFighter = userinput.nextInt();
-				int computerChoice= (int)(Math.random()*3)+0;
 				
 				System.out.println("Your fighter is "+fighterinformation.fightInformation.get(userChooseFighter).getFighterName());
-				System.out.println("Your opponent is "+fighterinformation.fightInformation.get(computerChoice).getFighterName());
+				System.out.println("Your opponent is "+fighterinformation.fightInformation.get(computerFighterChoice).getFighterName());
 				System.out.println("Let's Fight!!!");
 				
 			}
 		private static void computerchooseactions()
 			{
-				int computerActions= (int)(Math.random()*2)+1;
-				if (computerActions==1)
-					{
-						int fighting=fighterinformation.fightInformation.get(0).getAttack();
-					}
+				
 			}
 		private static void playerchoosing()
 			{
+				int computerActions= (int)(Math.random()*2)+1;
+				switch(computerActions)
+				{
+					case 1:
+							{
+								computerDamage=fighterinformation.fightInformation.get(computerFighterChoice).getAttack();;
+								computerActionChoice="Attack";
+							}
+					case 2:
+						
+							{
+								computerDamage=fighterinformation.fightInformation.get(computerFighterChoice).getDefence();
+								computerActionChoice="Defence";
+							}
+				}
+				
 				Scanner userinput1 = new Scanner(System.in);
-				System.out.println("Do you want to fight or defnece?");
+				System.out.println("Do you want to fight or defence?");
 				System.out.println("(1) fight  (2) defence");
 				int userChooseAction = userinput1.nextInt();
 				
+				if(userChooseAction==1)
+					{
+						if(computerActions==1)
+							{
+								System.out.println("Computer choose fight and deals "+computerDamage+" points");
+								System.out.println("You deal "+fighterinformation.fightInformation.get(userChooseFighter).getAttack()+" points");
+								System.out.println(fighterinformation.fightInformation.get(userChooseFighter).getAttack()-computerDamage);
+								System.out.println("Your Hp was " + fighterinformation.fightInformation.get(userChooseFighter).getHp());
+							}
+						else
+							{
+								
+							}
+						
+					}
 				
 			}
 		
