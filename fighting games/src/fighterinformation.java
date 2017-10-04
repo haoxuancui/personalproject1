e304546623371f9b7f12a5a6066fe1d0ac69cb4e
haoxuan.cui <haoxuan.cui@mu-c1-16.mullen.pvt.k12.co.us>
2017-10-04 09:31:27 -0600

    lastlast

--- fighting games/src/fighterinformation.java
+++ fighting games/src/fighterinformation.java
@@ -7,7 +7,7 @@ public class fighterinformation
 			{
 				
 						
-						fightInformation.add(new fighterdatabase("bigboy", 12, 5, 5));
+						fightInformation.add(new fighterdatabase("bigboy", 12, 4, 6));
 						fightInformation.add(new fighterdatabase("assasin", 10, 7, 4));
 						fightInformation.add(new fighterdatabase("tank", 20, 3, 8));
 						
